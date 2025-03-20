fd5d57d314e9163aae879eaa6a6f2482925e5a4d
EasybuttonDev <qinghangcom@gmail.com>
2025-03-20 14:53:29 +0800

    test

---
+++ JalanPlus/JalanPlus/JalanPlus/JalanPlusApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct JalanPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea(.keyboard)
+        }
+    }
+}
